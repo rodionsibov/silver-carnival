4a76c388572528b16ebc557a80476a07480b1509
Rodion Sibov <rodionsibov@gmail.com>
2022-01-01 20:13:57 +0200

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,14 +18,18 @@ const addDislike = () => {
 </script>
 
 <template>
-  <div>
+  <div class="post">
     <div><strong>Title:</strong> A post about JavaScript</div>
-    <div><strong>Description:</strong> JavaScript is an universal programming language</div>
+    <div>
+      <strong>Description:</strong> JavaScript is an universal programming
+      language
+    </div>
   </div>
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
+.post {
+  padding: 15px;
+  border: 2px solid teal;
 }
 </style>

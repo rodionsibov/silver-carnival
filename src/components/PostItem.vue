e6ba7d1a1359001c957c01be14cdc6f299a97871
Rodion Sibov <rodionsibov@gmail.com>
2022-01-05 12:04:38 +0200

    Initial commit

---
+++ src/components/PostItem.vue
@@ -0,0 +1,37 @@
+<script setup>
+defineProps({
+  post: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="p-5 border-2 border-teal-500">
+    <div>
+      <strong>Title:</strong>
+      {{ post.title }}
+    </div>
+    <div>
+      <strong>Description:</strong>
+      {{ post.body }}
+    </div>
+    <hr class="mt-2">
+    <div class="space-x-2 mt-4">
+      <button>Delete</button>
+      <button>Edit</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+button {
+  padding: 5px;
+  border: 2px solid teal;
+}
+
+button:hover {
+    opacity: 0.8;
+}
+</style>

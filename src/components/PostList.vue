2be46aa5b8454d039e430b82a11e15eb517888ac
Rodion Sibov <rodionsibov@gmail.com>
2022-01-05 11:52:26 +0200

    update

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,20 +1,23 @@
 <script setup>
-import PostItem from './PostItem.vue'
+import PostItem from "./PostItem.vue";
 
 defineProps({
-  posts: Object
-})
-
+  posts: Object,
+});
 </script>
 
 <template>
   <div class="p-3 space-y-4">
-    <div class="p-5 border-2 border-teal-500" v-for="post in posts" :key="post.id">
-     <PostItem :post="post"/>
+    <h3 class="text-3xl">User's List</h3>
+    <div
+      class="p-5 border-2 border-teal-500"
+      v-for="post in posts"
+      :key="post.id"
+    >
+      <PostItem :post="post" />
     </div>
   </div>
 </template>
 
 <style scoped>
-
 </style>

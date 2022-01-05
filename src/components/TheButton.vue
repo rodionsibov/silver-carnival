68433bdd338d489bb006d7a4a262393ed6f8a74c
Rodion Sibov <rodionsibov@gmail.com>
2022-01-05 16:11:54 +0200

    init

---
+++ src/components/TheButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <button
+    class="
+      bg-white
+      px-2
+      py-1
+      rounded
+      shadow-md
+      hover:opacity-80
+      cursor-pointer
+    "
+  >
+  <slot></slot>
+  </button>
+</template>
---
+++ src/components/TheInput.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  modelValue: [String, Number],
+});
+
+const emit = defineEmits(['update'])
+
+const updateInput = (event) => {
+    emit('update:modelValue', event.target.value)
+}
+</script>
+
+<template>
+  <input :value="modelValue" @input="updateInput" type="text" class="p-2" />
+</template>

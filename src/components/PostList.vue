228d65e85f0d4dc3930a54d559cc703ea83c82a7
Rodion Sibov <rodionsibov@gmail.com>
2022-01-05 12:04:20 +0200

    update

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -9,13 +9,7 @@ defineProps({
 <template>
   <div class="p-3 space-y-4">
     <h3 class="text-3xl">User's List</h3>
-    <div
-      class="p-5 border-2 border-teal-500"
-      v-for="post in posts"
-      :key="post.id"
-    >
-      <PostItem :post="post" />
-    </div>
+    <PostItem v-for="post in posts" :key="post.id" :post="post" />
   </div>
 </template>
 

192f6310658d5a6121e02dc2aa7e8a6875774087
Rodion Sibov <rodionsibov@gmail.com>
2022-01-06 10:09:54 +0200

    init

---
+++ src/components/TheDialog.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  show: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <div v-if="show" class="fixed inset-0 bg-green-800/80 pt-10">
+    <slot></slot>
+  </div>
+</template>

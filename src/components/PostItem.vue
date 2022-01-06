a1b3cea3089e8371beceb63d66bbc250b48fa6d9
Rodion Sibov <rodionsibov@gmail.com>
2022-01-06 17:41:55 +0200

    update

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -13,9 +13,9 @@ const emit = defineEmits(['remove'])
 </script>
 
 <template>
-  <div class="p-5 border-2 border-teal-500 flex items-center justify-between">
+  <div class="p-5 border-2 border-teal-500 flex justify-between">
     <div class="basis-8/12 space-y-4">
-        <div>
+        <div class="text-2xl">
           <strong>Title:</strong>
           {{ post.title }}
         </div>
@@ -25,7 +25,7 @@ const emit = defineEmits(['remove'])
         </div>
     </div>
     <!-- <hr class="mt-2"> -->
-    <div class="space-x-2">
+    <div class="space-x-2 self-end">
         <TheButton @click="emit('remove', post)" class="bg-red-500 text-white">Delete</TheButton>
         <TheButton class="bg-blue-500 text-white">Edit</TheButton>
     </div>

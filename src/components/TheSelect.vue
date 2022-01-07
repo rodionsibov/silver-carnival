b0a09fb4631166014e7a9a89786b5c1a05c01b07
Rodion Sibov <rodionsibov@gmail.com>
2022-01-07 12:19:42 +0200

    init

---
+++ src/components/TheSelect.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+defineProps({
+    modelValue: {
+        type: String
+    },
+    options: {
+        type: Array,
+        default: () => []
+    }
+})
+
+</script>
+
+<template>
+    <select name="" id="" v-model="modelValue">
+        <option value="" disabled>Select from the list</option>
+        <option value="" >Select from the list</option>
+    </select>
+</template>

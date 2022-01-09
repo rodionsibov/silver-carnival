0c5d18a02952613339381bd28c3dfe1befda7dbc
Rodion Sibov <rodionsibov@gmail.com>
2022-01-09 13:10:46 +0200

    update

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -11,7 +11,7 @@ const emit = defineEmits(["remove"]);
 <template>
   <div v-if="posts.length > 0" class="p-3 space-y-4">
     <h3 class="text-3xl">Posts</h3>
-    <transition-group>
+    <transition-group name="post-list">
       <PostItem
         v-for="post in posts"
         :key="post.id"
@@ -28,4 +28,14 @@ const emit = defineEmits(["remove"]);
 </template>
 
 <style scoped>
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 1s ease;
+}
+
+.post-list-from,
+.post-list-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>

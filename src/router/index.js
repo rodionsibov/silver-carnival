40e5b49449f53621b92d9a6689da5d0db0540a90
Rodion Sibov <rodionsibov@gmail.com>
2022-01-13 17:30:35 +0200

    Initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,11 @@ const routes = [
         name: 'Posts',
         component: () => import('../views/PostPage.vue'),
     },
+    {
+        path: '/about',
+        name: 'About',
+        component: () => import('../views/About.vue'),
+    },
 ]
 
 const router = createRouter({

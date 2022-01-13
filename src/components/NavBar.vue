72dd0518e5bbc351e18ca591e500937e3aa4fc82
Rodion Sibov <rodionsibov@gmail.com>
2022-01-13 17:53:20 +0200

    init

---
+++ src/components/NavBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="space-x-4 text-center my-3">
+    <router-link to="/">Home</router-link>
+    <router-link to="/posts">Posts</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
